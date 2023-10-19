97b9da6d1aa40521ba7f9d7011d646180ec0e9aa
Marcos <m.g4089188@gmail.com>
2023-10-19 19:08:51 -0300

    Database update

--- database/technician_test_multiline.sql
+++ database/technician_test_multiline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2023 a las 19:06:08
+-- Tiempo de generación: 20-10-2023 a las 00:08:09
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -40,16 +40,16 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`cliente_id`, `cliente_nombre`, `cliente_correo`, `created_at`, `updated_at`) VALUES
-(1, 'Lowe-Ernser', 'branson86@emard.org', NULL, NULL),
-(2, 'Upton LLC', 'rohan.jaden@aufderhar.com', NULL, NULL),
-(3, 'Ferry, Ziemann and Keeling', 'kfay@luettgen.biz', NULL, NULL),
-(4, 'Jaskolski-Cassin', 'marguerite.bednar@brakus.com', NULL, NULL),
-(5, 'Carroll-Gerlach', 'phirthe@blick.com', NULL, NULL),
-(6, 'Dietrich and Sons', 'roberts.tressa@kautzer.net', NULL, NULL),
-(7, 'Wunsch LLC', 'grunte@bruen.com', NULL, NULL),
-(8, 'Gulgowski and Sons', 'lwehner@lindgren.com', NULL, NULL),
-(9, 'Stiedemann, Brakus and Harris', 'adriana.lakin@bode.com', NULL, NULL),
-(10, 'Breitenberg Ltd', 'mariana94@koepp.com', NULL, NULL);
+(1, 'Feeney-Willms', 'ashlynn.tromp@ullrich.com', NULL, NULL),
+(2, 'Bogisich Group', 'zackary.hahn@rempel.com', NULL, NULL),
+(3, 'Langworth, Crist and Parker', 'clay23@moen.com', NULL, NULL),
+(4, 'Krajcik, Jaskolski and Medhurst', 'collin29@adams.org', NULL, NULL),
+(5, 'Greenfelder, Upton and Romaguera', 'wmetz@romaguera.org', NULL, NULL),
+(6, 'Beier, Beahan and Beahan', 'srunte@aufderhar.info', NULL, NULL),
+(7, 'Simonis, Ruecker and Cronin', 'pkoelpin@olson.com', NULL, NULL),
+(8, 'Predovic PLC', 'dusty.brekke@hammes.info', NULL, NULL),
+(9, 'Mosciski, Legros and Luettgen', 'danielle.cassin@jerde.info', NULL, NULL),
+(10, 'Lueilwitz, Gleichner and Willms', 'jayme14@collier.biz', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -66,6 +66,29 @@ CREATE TABLE `llamada` (
   `llamada_observacion` text NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `llamada`
+--
+
+INSERT INTO `llamada` (`llamada_id`, `cliente_id`, `user_id`, `llamada_telefono`, `llamada_fecha`, `llamada_observacion`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '099999999', '2023-10-19 13:41:47', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', '2023-10-19 16:41:47', '2023-10-19 16:41:47'),
+(2, 2, 1, '098888888', '2023-10-19 13:42:53', 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo \"Contenido aquí, contenido aquí\". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de \"Lorem Ipsum\" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).', '2023-10-19 16:42:53', '2023-10-19 16:42:53'),
+(3, 3, 2, '097777777', '2023-10-19 13:43:45', 'Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo, haciendo que este adquiera mas de 2000 años de antiguedad.', '2023-10-19 16:43:45', '2023-10-19 16:43:45'),
+(4, 3, 1, '096666666', '2023-10-19 13:48:05', 'El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de \"de Finibus Bonorum et Malorum\" por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.', '2023-10-19 16:48:05', '2023-10-19 16:48:05'),
+(5, 4, 2, '095555555', '2023-10-19 18:41:47', 'Prueba de observación', '2023-10-19 21:41:47', '2023-10-19 21:41:47');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -90,7 +113,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Marcos', 'marcos@prueba.com', NULL, '$2y$10$ISoy2rpXtcPnJMdKw27DFOSFAzrMDqmWOw5InJxl9j60hlUAVS4Sm', NULL, '2023-10-18 17:50:18', '2023-10-18 17:50:18');
+(1, 'Marcos', 'marcos@prueba.com', NULL, '$2y$10$O8r78LA/w6FjAP.62uytpOCjA4HQHChmN1F2dwZV6hKy9VOIEDmBK', NULL, '2023-10-19 16:41:23', '2023-10-19 16:41:23'),
+(2, 'Prueba', 'prueba@prueba.com', NULL, '$2y$10$T0XWUd1chPmy6mgrv2qvzO4Hs.RtHcmHFTZhif5KUs7lXbTHMtpgq', NULL, '2023-10-19 21:43:23', '2023-10-19 21:43:23');
 
 --
 -- Índices para tablas volcadas
@@ -109,6 +133,12 @@ ALTER TABLE `llamada`
   ADD PRIMARY KEY (`llamada_id`),
   ADD KEY `llamada_cliente_id_foreign` (`cliente_id`),
   ADD KEY `llamada_user_id_foreign` (`user_id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
 
 --
 -- Indices de la tabla `users`
@@ -131,13 +161,13 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de la tabla `llamada`
 --
 ALTER TABLE `llamada`
-  MODIFY `llamada_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `llamada_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
